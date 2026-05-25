e1a6b8dcd0f8449625c373cc64f1faa928a351c0
Daniel Beliavskij <daniel.beliavskij@axioma.eu>
2026-05-25 21:24:45 +0300

    Initial commit

---
+++ cmake/toolchain/stm32l5-gcc.cmake
@@ -0,0 +1,22 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR cortex-m33)
+
+find_program(CMAKE_C_COMPILER NAMES arm-none-eabi-gcc)
+find_program(CMAKE_ASM_COMPILER NAMES arm-none-eabi-gcc)
+find_program(CMAKE_AR NAMES arm-none-eabi-ar)
+find_program(CMAKE_RANLIB NAMES arm-none-eabi-ranlib)
+find_program(CMAKE_OBJCOPY NAMES arm-none-eabi-objcopy)
+find_program(CMAKE_SIZE NAMES arm-none-eabi-size)
+find_program(CMAKE_GDB NAMES arm-none-eabi-gdb)
+
+set(CMAKE_EXECUTABLE_SUFFIX ".elf")
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+set(CMAKE_C_FLAGS_INIT "-mcpu=cortex-m33 -mthumb -mfpu=fpv5-sp-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -Wall -Wextra -Werror")
+set(CMAKE_ASM_FLAGS_INIT "-mcpu=cortex-m33 -mthumb -mfpu=fpv5-sp-d16 -mfloat-abi=hard -x assembler-with-cpp")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-mcpu=cortex-m33 -mthumb -mfpu=fpv5-sp-d16 -mfloat-abi=hard --specs=nano.specs --specs=nosys.specs -Wl,--gc-sections -Wl,--print-memory-usage")
